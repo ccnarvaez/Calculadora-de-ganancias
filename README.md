faf07d2a1e38f45fc2c62fdb3ed7cadda715129b
ccnarvaez <ccnarvaez90@gmail.com>
2021-07-29 22:40:46 -0400

    code cleaned

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Calculadora-de-ganancias
+# Calculadora de ganancias
+
+Invertir es una actividad que supone cierto riesgo, y para hacerla en forma efectiva es necesario hacer un proceso de investigación que permita conocer y desarrollar cierto grado de familiarización con los mercados que estamos tratando.
+
+Un fondo de inversión reúne dinero de inversionistas que delegan la administración de su dinero a un profesional, quien toma decisiones diarias sobre la compra de valores que produzcan rendimientos que busquen obtener los objetivos del fondo, sin embargo, no es sencillo delegar nuestro dinero en manos de desconocidos.
+
+Por tal motivo, surge la necesidad de desarrollar una herramienta que permita estudiar el comportamiento de los fondos de inversión en un rango de tiempo, a fin de evaluar el comportamiento del mercado y definir la mejor estrategia de inversión.
+
+# Fondos de inversión
+
+Para el desarrollo de este programa fueron consumidos los datos de la API de fintual (https://fintualist.com/chile/noticias/el-api-de-fintual/), y se consideran tres fondos de inversión:
+
+* Risky Morris
+* Moderate Pitt
+* Conservative Clooney
+
+# ¿Cómo funciona?
+El programa simula un proceso de inversión en la que, se evaluará el crecimiento (o pérdidas) en términos monetarios de una inversión como si hubiese sido realizada en alguna fecha del pasado.
+
+Se desarrolló un programa de consola, el cuál solicitará los siguientes datos para su funcionamiento:
+
+* Fecha de inversión: Se debe indicar una fecha (algún día del pasado) a partir del cual se desee evaluar el comportamiento del fondo de inversión. La fecha debe ingresarse en formato DD/MM/AAAA.
+
+* Total del dinero a invertir: Aquí se debe declarar la cantidad de dinero sobre la que se desea simular la inversión. La cantidad debe declararse en pesos chilenos.
+
+* Porcentajes en cada fondo: Se debe indicar que porcentaje del dinero total desea invertirse en cada uno de los fondos. Esta cantidad debe ser escrita en porcentaje Ej. 20 (no olvides que la suma de porcentajes de los tres fondos debe ser igual al 100%)
+
+# Respuesta
+Una vez ingresados los datos solicitados, el programa emitirá una respuesta:
+
+* Crecimiento de la inversión: factor que relaciona el crecimiento del valor de la acción desde el día evaluado hasta la fecha.
+
+* Dinero total por fondo: Muestra la cantidad de dinero que dispone luego de la inversión.
+
+
+
+
--- index.rb
+++ index.rb
@@ -1,8 +1,13 @@
 require 'json'
 require 'rest-client'
-# ==VERSIÓN 1 (DEPRECATED)
-# Esta fue la primera versión desarrollada para el problema planteado. Esta solución fue sustituída porla versión 2
 
+# VERSIÓN 2 
+=begin 
+En esta versión se incluyeron mejoras en el desarrollo del código, 
+generalizando mediante un ciclo las variables requeridas para consumir la API.
+Tambien se efectuo una generalizacion de las variables asociadas a las fechas 
+(tanto la de consulta input_date como la de referencia )
+=end
 # INPUT VALUES
 print "Escriba fecha de consulta en formato DD/MM/AAAA"
 input_date = (Date.parse(gets.chomp.to_s)).to_s
@@ -28,104 +33,108 @@ accum = 0
 ids = ['186','187','188']
 urls =[]
 inv_date =[]
-reference_date = []
+ref_date = []
 
-ids.each do |id|
-     
+ids.each do |id|  
     urls[i] = 'https://fintual.cl/api/real_assets/'+ id +'/days?from_date='+ input_date
     inv_date[i] = id +'-'+ input_date
     time = Time.new
-    reference_date[i] = id +'-'+ time.year.to_s + '-' + time.month.to_s.rjust(2, "0") + '-' + ((time.day-1).to_s).rjust(2, "0")
+    ref_date[i] = id +'-'+ time.year.to_s + '-' + time.month.to_s.rjust(2, "0") + '-' + ((time.day-1).to_s).rjust(2, "0")
     i = i+1
 end
 
 #RISKY MORRIS
-risky_morris_url = 'https://fintual.cl/api/real_assets/186/days?from_date='+ input_date
-risky_morris_inv_date = '186-'+ input_date
-time = Time.new
-reference_date = '186-'+ time.year.to_s + '-' + time.month.to_s.rjust(2, "0") + '-' + ((time.day-1).to_s).rjust(2, "0")
+url = urls[0]
+investment_date = inv_date[0]
+reference_date = ref_date[0]
 
 # API data
-risky_morris_response = RestClient.get(risky_morris_url)
-results = JSON.parse(risky_morris_response)
-
-# Getting info from API
+response = RestClient.get(url)
+results = JSON.parse(response)
 puts 'Inversion inicial en fondo risky morris (pesos)'
-puts risky_morris_money = input_money*investment_fund['risky morris'] 
-counter = 0
-results["data"].each do |result|
-    if result["id"] == risky_morris_inv_date 
-        value = result["attributes"]["price"].to_f     
-    end
-    if result["id"] == reference_date
-        value_ref = result["attributes"]["price"].to_f   
-    end
-    if value != '' && value_ref != '' && counter != 1
-        benefit = (value_ref - value)/value_ref
-        puts 'Saldo + Ganancia risky morris (pesos)'
-        puts money_increase = (risky_morris_money*benefit) + risky_morris_money
-        accum = money_increase
-        counter = counter +1
-    end
-    i = i+1
-end
-
-# MODERATE PITT
-moderate_pitt_url = 'https://fintual.cl/api/real_assets/187/days?from_date='+ input_date
-moderate_pitt_inv_date = '187-'+ input_date
-
-# API data
-moderate_pitt_response = RestClient.get(moderate_pitt_url)
-results = JSON.parse(moderate_pitt_response)
+puts initial_money = input_money*investment_fund['risky morris'] 
 
 # Getting info from API
-puts 'Inversion inicial en fondo Moderate Pitt (pesos)'
-puts moderate_pitt_money = input_money*investment_fund['moderate pitt']
 counter = 0
 results["data"].each do |result|
-    if result["id"] == moderate_pitt_inv_date 
+    if result["id"] == investment_date
         value = result["attributes"]["price"].to_f     
     end
     if result["id"] == reference_date
         value_ref = result["attributes"]["price"].to_f   
     end
     if value != '' && value_ref != '' && counter != 1
-        benefit = (value_ref - value)/value_ref
-        puts 'Saldo + Ganancia Moderate Pitt (pesos)'
-        puts money_increase = (moderate_pitt_money*benefit )+ moderate_pitt_money
+        puts 'Crecimiento de la inversion'
+        puts benefit = (value_ref - value)/value_ref
+        puts 'Saldo + Ganancia risky morris (pesos)'
+        puts money_increase = (initial_money*benefit) + initial_money
+        
         accum = accum + money_increase
         counter = counter +1
     end
     i = i+1
 end
 
-# CONSERVATIVE CLOONEY
-conservative_clooney_url = 'https://fintual.cl/api/real_assets/188/days?from_date='+ input_date
-conservative_clooney_inv_date = '188-'+ input_date
+#MODERATE PITT
+url = urls[1]
+investment_date = inv_date[1]
+reference_date = ref_date[1]
 
 # API data
-conservative_clooney_response = RestClient.get(conservative_clooney_url)
-results = JSON.parse(conservative_clooney_response)
+response = RestClient.get(url)
+results = JSON.parse(response)
+puts 'Inversion inicial en fondo moderate pitt (pesos)'
+puts initial_money = input_money*investment_fund['moderate pitt'] 
 
 # Getting info from API
-
-puts 'Inversion inicial en fondo Conservative Clooney (pesos)'
-puts conservative_clooney_money = input_money*investment_fund['conservative clooney'] 
 counter = 0
 results["data"].each do |result|
-    if result["id"] == conservative_clooney_inv_date 
+    if result["id"] == investment_date
         value = result["attributes"]["price"].to_f     
     end
     if result["id"] == reference_date
         value_ref = result["attributes"]["price"].to_f   
     end
     if value != '' && value_ref != '' && counter != 1
+        puts 'Crecimiento de la inversion'
         puts benefit = (value_ref - value)/value_ref
-        puts 'Saldo + Ganancia Conservative Clooney (pesos)'
-        puts money_increase = (conservative_clooney_money*benefit)+ conservative_clooney_money
-        
-        puts 'Saldo total (tres fondos)'
-        puts accum = accum + money_increase
+        puts 'Saldo + Ganancia moderate pitt (pesos)'
+        puts money_increase = (initial_money*benefit) + initial_money
+
+        accum = accum+ money_increase
+        counter = counter +1
+    end
+    i = i+1
+end
+
+#CONSERVATIVE CLOONEY
+url = urls[2]
+investment_date = inv_date[2]
+reference_date = ref_date[2]
+
+# API data
+response = RestClient.get(url)
+results = JSON.parse(response)
+puts 'Inversion inicial en fondo conservative clooney (pesos)'
+puts initial_money = input_money*investment_fund['conservative clooney'] 
+
+# Getting info from API
+counter = 0
+results["data"].each do |result|
+    if result["id"] == investment_date
+        value = result["attributes"]["price"].to_f     
+    end
+    if result["id"] == reference_date
+        value_ref = result["attributes"]["price"].to_f   
+    end
+    if value != '' && value_ref != '' && counter != 1
+        puts 'Crecimiento de la inversion'
+        puts benefit = (value_ref - value)/value_ref
+        puts 'Saldo + Ganancia conservative clooney (pesos)'
+        puts money_increase = (initial_money*benefit) + initial_money
+
+        puts 'El saldo total en los tres fondos es de: '
+        puts accum = accum+ money_increase
         counter = counter +1
     end
     i = i+1
